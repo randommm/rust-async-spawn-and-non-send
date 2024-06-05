a266c621c2d48862d411e229f1709c5187203c2f
Marco Inacio <m@marcoinacio.com>
2024-06-05 12:19:31 +0100

    simplifications

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,23 @@
+use rand::{thread_rng, Rng};
 use std::rc::Rc;
+use tokio::task::JoinSet;
 
-async fn task_with_blocking_sleeps(id: i32) {
-    println!("Task {} started", id);
-    let tid = std::thread::current().id();
-    std::thread::sleep(std::time::Duration::from_secs(3));
-    println!(
-        "Blocking sleep task {} started on {:?} and completed on {:?}",
-        id,
-        tid,
-        std::thread::current().id(),
-    );
+fn get_random_number() -> u64 {
+    thread_rng().gen_range(100..250)
 }
 
 async fn task(id: i32) {
     println!("Task {} started", id);
     let tid = std::thread::current().id();
-    tokio::time::sleep(std::time::Duration::from_secs(3)).await;
+
+    // async sleep, task yields thread back to async runtime
+    tokio::time::sleep(std::time::Duration::from_millis(get_random_number())).await;
+
+    // blocking sleep, task does **not** yield thread back to async runtime
+    std::thread::sleep(std::time::Duration::from_millis(get_random_number()));
+
     println!(
-        "Async sleep task {} started on {:?} and completed on {:?}",
+        "Task {} started on {:?} and completed on {:?}",
         id,
         tid,
         std::thread::current().id(),
@@ -26,22 +26,21 @@ async fn task(id: i32) {
 
 async fn hold_non_send() {
     let rc = Rc::new(1);
-    // uncomment to cause compilation error
-    // tokio::time::sleep(std::time::Duration::from_secs(1)).await;
-    println!("rc: {:?}", rc);
+    // uncomment to cause a compilation error
+    // tokio::time::sleep(std::time::Duration::from_millis(get_random_number())).await;
+    let _ = rc.clone();
 }
 
 // #[tokio::main(flavor = "current_thread")]
 #[tokio::main(flavor = "multi_thread")]
 async fn main() {
     println!("Started main function on {:?}", std::thread::current().id(),);
-    for i in 0..10 {
-        tokio::spawn(task(i));
-    }
-    for i in 10..20 {
-        tokio::spawn(task_with_blocking_sleeps(i));
-    }
 
-    tokio::spawn(hold_non_send()).await;
-    tokio::time::sleep(std::time::Duration::from_secs(10000)).await;
+    let mut set = JoinSet::new();
+    for i in 0..10 {
+        set.spawn(task(i));
+    }
+    while set.join_next().await.is_some() {}
+
+    tokio::spawn(hold_non_send()).await.unwrap();
 }
